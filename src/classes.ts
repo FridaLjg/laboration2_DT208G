f70bd7c609194192ac1e234cad9168d433ca6944
Frida Ljungberg <frlj2501@student.miun.se>
2026-04-13 19:07:53 +0200

    Lagt till så todos sparas till localStorage

--- src/classes.ts
+++ src/classes.ts
@@ -5,21 +5,38 @@ interface Todo {
 }
 
 class TodoList {
-    todos: Todo[] = []
+    todos: Todo[] = [];
+
+    //Ladda sparade todos från localStorage
+    storageKey: string;
+
+    constructor(storageKey = "todos") {
+        this.storageKey = storageKey;
+        this.todos = [];
+        this.loadFromLocalStorage();
+    }
+
+    loadFromLocalStorage(): void {
+        const storedItems = localStorage.getItem(this.storageKey);
+        
+        if (storedItems) {
+            this.todos = JSON.parse(storedItems);
+        }
+    }
 
     //Metod som lägger till nya "todos"
     addTodo(task: string, priority: number): boolean {
-        if(task.trim().length === 0) {
+        if (task.trim().length === 0) {
             return false;
         }
 
-    this.todos.push({
-        task,
-        completed: false,
-        priority,
-    })
+        this.todos.push({
+            task,
+            completed: false,
+            priority,
+        })
 
-    return true;
+        return true;
     }
 
     //Metod som markerar todos som klara
@@ -28,5 +45,19 @@ class TodoList {
             this.todos[todoIndex].completed = true;
         }
     }
+
+    //Metod som hämtar hela listan av todos
+    getTodos(): Todo[] {
+        return this.todos;
+    }
+
+    //Metod somn sparar todos till localStorage
+    saveToLocalStorage(): void {
+        localStorage.setItem(
+            this.storageKey,
+            JSON.stringify(this.todos)
+        );
+    }
+
 }
 
